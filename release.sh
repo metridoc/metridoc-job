b285dc2cafa93475297e13bef17edbecf51f28bc
Tommy Barker <thomas.p.barker@gmail.com>
2013-10-23 21:38:07 -0400

    consolidate common build
    moved all common build to parent project

---
+++ release.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+systemCall() {
+    echo "running $1"
+    if eval $1; then
+		echo "command [$1] ran successfully"
+	else
+		echo "command [$1] failed with exit status [$?]"
+		exit 1
+	fi
+}
+
+if grep -q "\-SNAPSHOT" "build.gradle"; then
+    echo "build file has SNAPSHOT in it, skipping release"
+    exit 0
+fi
+
+if grep -q "\-SNAPSHOT" "VERSION"; then
+    echo "VERSION file has SNAPSHOT in it, skipping release"
+    exit 0
+fi
+
+systemCall "./gradlew test"
+
+#releases to github
+PROJECT_VERSION=`cat VERSION`
+echo ""
+echo "Releasing ${PROJECT_VERSION} to GitHub"
+echo ""
+
+systemCall "git tag -a v${PROJECT_VERSION} -m 'tagging release'"
+systemCall "git push origin v${PROJECT_VERSION}"
+
+#release to bintray
+echo ""
+echo "Releasing ${PROJECT_VERSION} to BinTray"
+echo ""
+
+systemCall "./gradlew publishArchives publishDistribution bumpVersion"
+systemCall "git add VERSION"
+systemCall "git commit -m 'committing a new version'"
+systemCall "git push origin master"
