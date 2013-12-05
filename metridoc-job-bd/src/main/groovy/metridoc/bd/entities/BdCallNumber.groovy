59498bfc2d2e0764cf18e2f3630fd516e2f03db1
tbarker <mingus.karate@gmail.com>
2013-12-05 16:14:51 -0500

    add BdCallNumber

---
+++ metridoc-job-bd/src/main/groovy/metridoc/bd/entities/BdCallNumber.groovy
@@ -0,0 +1,30 @@
+package metridoc.bd.entities
+
+import grails.persistence.Entity
+
+/**
+ * Created by tbarker on 12/5/13.
+ */
+@Entity
+class BdCallNumber {
+    Long callNumberId
+    String requestNumber
+    Boolean holdingsSeq
+    String supplierCode
+    String callNumber
+    Date processDate
+    Date loadTime
+
+    static constraints = {
+        requestNumber(nullable: true, maxSize: 12)
+        holdingsSeq(nullable: true)
+        supplierCode(nullable: true, maxSize: 20)
+        callNumber(nullable: true)
+        processDate(nullable: true)
+    }
+
+    static mapping = {
+        id name: callNumberId
+        requestNumber index: "request_number"
+    }
+}
