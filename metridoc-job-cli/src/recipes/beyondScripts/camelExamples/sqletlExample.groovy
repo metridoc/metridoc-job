c21e92c02fd122c9325ee6b5a5342b849bfa63ad
tbarker <mingus.karate@gmail.com>
2014-02-05 14:06:53 -0500

    add camelService example

---
+++ metridoc-job-cli/src/recipes/beyondScripts/camelExamples/sqletlExample.groovy
@@ -0,0 +1,25 @@
+import metridoc.utils.DataSourceConfigUtil
+import groovy.sql.Sql
+import metridoc.core.services.CamelService
+
+/*
+  make sure you don't use def fooBarDataSource, otherwise the variable will not be bound and usable by sqletl
+*/
+fooBarDataSource = DataSourceConfigUtil.embeddedDataSource
+def sql = new Sql(fooBarDataSource)
+
+def camelService = includeService(CamelService)
+
+sql.execute("create table foo (foobar int)")
+sql.execute("create table bar (foobar int)")
+sql.execute("insert into foo values (1)")
+sql.execute("insert into foo values (2)")
+sql.execute("insert into foo values (3)")
+
+assert 0 == sql.firstRow("select count(*) as total from bar").total
+
+camelService.consume("sqletl://FOO?dataSource=fooBarDataSource") {resultSet ->
+	camelService.send("sqletl://BAR?dataSource=fooBarDataSource", resultSet)
+}
+
+assert 3 == sql.firstRow("select count(*) as total from bar").total
