e4eaa4802b88b0fea5a2ebdd5635ef2238a9dc62
TApicella <tapicella.ms@gmail.com>
2015-09-09 10:46:03 -0400

    Finish adding EZB exception handling

--- metridoc-job-bd/src/main/groovy/metridoc/bd/utils/Validator.groovy
+++ metridoc-job-bd/src/main/groovy/metridoc/bd/utils/Validator.groovy
@@ -141,7 +141,7 @@ class Validator {
                             log.warn "*******************"
                             log.warn "Problem entry is ${entry}"
                             log.warn "Problem key is ${key}"
-                            /*entries.add("'"+entry+"'")
+                            entries.add("'"+entry+"'")
                             duplicateExceptions = "h.request_number not in ("
                             duplicateExceptions += entries.join(", ")
                             duplicateExceptions += ") and "
@@ -149,10 +149,10 @@ class Validator {
                             sqlStmt = "select s.library_id as lender,r.library_id as borrower,r.request_number, " +
                                     " abs(cast(HASHBYTES('md5',p.patron_id) as int)) as patron_id,p.patron_type,r.author, " +
                                     "r.title,r.local_item_found,r.publisher,r.publication_place,r.publication_year,r.edition,r.isbn,r.isbn_2, " +
-                                    "r.bibliography_num as LCCN,system_number as oclc_text,r.date_entered as request_date,d.date_processed as process_date, " +
+                                    "r.bibliography_num as LCCN,oclc_num as oclc,r.date_entered,r.date_entered as request_date,d.date_processed as process_date, " +
                                     "pl.pickup_location_desc as pickup_location,d.supplier_code_1 as supplier_code,h.call_number from " +
                                     specification.sourceTables + " where " + specification.sourceFilter +duplicateExceptions+specification.sourceGroup + " = '${date}'"
-                            */
+                            
                             throw e;
 
                         }
